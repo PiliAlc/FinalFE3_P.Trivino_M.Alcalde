8893434e0a67aa5199a229eae8e7f450d591557c
PilarAlcalde <pilar-alcalde@hotmail.com>
2023-09-15 13:50:27 -0300

    commit version final

---
+++ Final_FFE3/src/Routes/Contact.jsx
@@ -0,0 +1,13 @@
+import Form from '../components/Form'
+
+const Contact = () => {
+  return (
+    <div className='contacto'>
+      <h2>Contacto</h2>
+      <h2>Envíanos tu consulta, a la brevedad te responderemos</h2>
+      <Form/>
+    </div>
+  )
+}
+
+export default Contact
